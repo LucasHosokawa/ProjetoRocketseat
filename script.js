ba2c1e6462a4bd72e611fddd5b3bcc354066d57a
“LucasHosokawa <lucasyoshihiro@hotmail.com>
2025-05-28 10:43:39 -0300

    projeto final

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-function toggleMode () {
+function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
 
   const img = document.querySelector("#profile img")
-if (html.classList.contains("light")) {
-  img.setAttribute("src", "./assets/avatar-light.png")
-} else {
-  img.setAttribute("src", "./assets/avatar.png")
-}
-
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/Avatar-light.jpeg")
+    img.setAttribute(
+      "alt",
+      "Foto de Lucas Hosokawa sorrindo, no espelho com camiseta cinza."
+    )
+  } else {
+    img.setAttribute("src", "./assets/Avatar.jpeg")
+    img.setAttribute(
+      "alt",
+      "Foto de Lucas Hosokawa sorrindo, usando camiseta branca."
+    )
+  }
 }
